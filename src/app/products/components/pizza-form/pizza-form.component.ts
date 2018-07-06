a4104b1ff9805a6307ed0163159801f31ede0a5e
Alexey <asdalexey@yandex.ru>
2018-07-06 17:17:34 +0300

    #26 small changes

--- src/app/products/components/pizza-form/pizza-form.component.ts
+++ src/app/products/components/pizza-form/pizza-form.component.ts
@@ -26,11 +26,11 @@ import { Topping } from '../../models/topping.model';
   template: `
     <div class="pizza-form">
       <form [formGroup]="form">
-      
+
         <label>
           <h4>Pizza name</h4>
-          <input 
-            type="text" 
+          <input
+            type="text"
             formControlName="name"
             placeholder="e.g. Pepperoni"
             class="pizza-form__input"
@@ -41,7 +41,7 @@ import { Topping } from '../../models/topping.model';
             <p>Pizza must have a name</p>
           </div>
         </label>
-      
+
         <ng-content></ng-content>
 
         <label>
@@ -102,7 +102,8 @@ export class PizzaFormComponent implements OnChanges {
     toppings: [[]],
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+  }
 
   get nameControl() {
     return this.form.get('name') as FormControl;
@@ -118,11 +119,11 @@ export class PizzaFormComponent implements OnChanges {
       this.form.patchValue(this.pizza);
     }
     this.form
-      .get('toppings')
-      .valueChanges.pipe(
-        map(toppings => toppings.map((topping: Topping) => topping.id))
-      )
-      .subscribe(value => this.selected.emit(value));
+        .get('toppings')
+        .valueChanges.pipe(
+      map(toppings => toppings.map((topping: Topping) => topping.id))
+    )
+        .subscribe(value => this.selected.emit(value));
   }
 
   createPizza(form: FormGroup) {
