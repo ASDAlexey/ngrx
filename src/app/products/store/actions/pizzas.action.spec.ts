e6429918a53792e6b9ea931680cee9c775da886b
Alexey <asdalexey@yandex.ru>
2018-07-08 13:52:26 +0300

    #38 add tests

--- src/app/products/store/actions/pizzas.action.spec.ts
+++ src/app/products/store/actions/pizzas.action.spec.ts
@@ -1,4 +1,16 @@
-import { LOAD_PIZZAS, LOAD_PIZZAS_FAIL, LOAD_PIZZAS_SUCCESS, LoadPizzas, LoadPizzasFail, LoadPizzasSuccess } from '@app/products/store';
+import {
+  CREATE_PIZZA, CREATE_PIZZA_FAIL, CREATE_PIZZA_SUCCESS,
+  CreatePizza, CreatePizzaFail, CreatePizzaSuccess,
+  LOAD_PIZZAS,
+  LOAD_PIZZAS_FAIL,
+  LOAD_PIZZAS_SUCCESS,
+  LoadPizzas,
+  LoadPizzasFail,
+  LoadPizzasSuccess,
+  REMOVE_PIZZA, REMOVE_PIZZA_FAIL, REMOVE_PIZZA_SUCCESS, RemovePizza,
+  RemovePizzaFail,
+  RemovePizzaSuccess, UPDATE_PIZZA, UPDATE_PIZZA_FAIL, UPDATE_PIZZA_SUCCESS, UpdatePizza, UpdatePizzaFail, UpdatePizzaSuccess
+} from '@app/products/store';
 
 describe('Pizzas Action', () => {
   describe('LoadPizzas Actions', () => {
@@ -118,6 +130,167 @@ describe('Pizzas Action', () => {
 
         const action = new LoadPizzasSuccess(payload);
         expect({ ...action }).toEqual({ type: LOAD_PIZZAS_SUCCESS, payload });
+      });
+    });
+  });
+
+  describe('CreatePizza Actions', () => {
+    describe('CreatePizza', () => {
+      it('should create an action', () => {
+        const payload = {
+          name: 'Pizza #2',
+          toppings: [
+            { id: 1, name: 'onion' },
+            { id: 2, name: 'mushroom' },
+            { id: 3, name: 'basil' },
+          ],
+        };
+        const action = new CreatePizza(payload);
+
+        expect({ ...action }).toEqual({
+          type: CREATE_PIZZA,
+          payload,
+        });
+      });
+    });
+
+    describe('CreatePizzaFail', () => {
+      it('should create an action', () => {
+        const payload = { message: 'Create Error' };
+        const action = new CreatePizzaFail(payload);
+
+        expect({ ...action }).toEqual({
+          type: CREATE_PIZZA_FAIL,
+          payload,
+        });
+      });
+    });
+
+    describe('CreatePizzaSuccess', () => {
+      it('should create an action', () => {
+        const payload = {
+          id: 2,
+          name: 'Pizza #2',
+          toppings: [
+            { id: 1, name: 'onion' },
+            { id: 2, name: 'mushroom' },
+            { id: 3, name: 'basil' },
+          ],
+        };
+        const action = new CreatePizzaSuccess(payload);
+
+        expect({ ...action }).toEqual({
+          type: CREATE_PIZZA_SUCCESS,
+          payload,
+        });
+      });
+    });
+  });
+
+  describe('UpdatePizza Actions', () => {
+    describe('UpdatePizza', () => {
+      it('should create an action', () => {
+        const payload = {
+          id: 2,
+          name: 'Pizza #2',
+          toppings: [
+            { id: 1, name: 'onion' },
+            { id: 2, name: 'mushroom' },
+            { id: 3, name: 'basil' },
+          ],
+        };
+        const action = new UpdatePizza(payload);
+
+        expect({ ...action }).toEqual({
+          type: UPDATE_PIZZA,
+          payload,
+        });
+      });
+    });
+
+    describe('UpdatePizzaFail', () => {
+      it('should create an action', () => {
+        const payload = { message: 'Update Error' };
+        const action = new UpdatePizzaFail(payload);
+
+        expect({ ...action }).toEqual({
+          type: UPDATE_PIZZA_FAIL,
+          payload,
+        });
+      });
+    });
+
+    describe('UpdatePizzaSuccess', () => {
+      it('should create an action', () => {
+        const payload = {
+          id: 2,
+          name: 'Pizza #2',
+          toppings: [
+            { id: 1, name: 'onion' },
+            { id: 2, name: 'mushroom' },
+            { id: 3, name: 'basil' },
+          ],
+        };
+        const action = new UpdatePizzaSuccess(payload);
+
+        expect({ ...action }).toEqual({
+          type: UPDATE_PIZZA_SUCCESS,
+          payload,
+        });
+      });
+    });
+  });
+
+  describe('RemovePizza Actions', () => {
+    describe('RemovePizza', () => {
+      it('should create an action', () => {
+        const payload = {
+          id: 2,
+          name: 'Pizza #2',
+          toppings: [
+            { id: 1, name: 'onion' },
+            { id: 2, name: 'mushroom' },
+            { id: 3, name: 'basil' },
+          ],
+        };
+        const action = new RemovePizza(payload);
+
+        expect({ ...action }).toEqual({
+          type: REMOVE_PIZZA,
+          payload,
+        });
+      });
+    });
+
+    describe('RemovePizzaFail', () => {
+      it('should create an action', () => {
+        const payload = { message: 'Remove Error' };
+        const action = new RemovePizzaFail(payload);
+
+        expect({ ...action }).toEqual({
+          type: REMOVE_PIZZA_FAIL,
+          payload,
+        });
+      });
+    });
+
+    describe('RemovePizzaSuccess', () => {
+      it('should create an action', () => {
+        const payload = {
+          id: 2,
+          name: 'Pizza #2',
+          toppings: [
+            { id: 1, name: 'onion' },
+            { id: 2, name: 'mushroom' },
+            { id: 3, name: 'basil' },
+          ],
+        };
+        const action = new RemovePizzaSuccess(payload);
+
+        expect({ ...action }).toEqual({
+          type: REMOVE_PIZZA_SUCCESS,
+          payload,
+        });
       });
     });
   });
